f695f9a4daf32017a0b02d220e32c41101ee6ebe
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-06-05 21:29:33 +0100

    :card_file_box: connected MongoDB with API Express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./src/routes/user.route.js";
+import connectDatabase from "./src/database/db.js";
 
 const app = express();
 
 const port = 3000;
 
+connectDatabase();
 app.use(express.json());
 app.use("/user", userRouter);
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,22 @@
-const create = (req, res) => {
+import userService from "../services/user.service.js";
+
+const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
 
   if (!name || !username || !email || !password || !avatar || !background) {
     res.status(400).send({ message: "Submit all fields for registration" });
   }
 
+  const user = await userService.create(req.body);
+
+  if (!user) {
+    return res.status(500).send({ message: "Error creating user" });
+  }
+
   res.status(201).send({
     message: "User created successfully",
     user: {
+      id: user._id,
       name,
       username,
       email,
